9f991fdb936fe5a50b36946211ff9248be41da7d
talladaakhil <talladaakhil@gmail.com>
2024-09-16 21:22:43 +0530

    removed space

--- SampleTest/src/com/te/Samp.java
+++ SampleTest/src/com/te/Samp.java
@@ -1,9 +1,7 @@
 package com.te;
 
 public class Samp {
-
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) {		
 		System.out.println("hello");
 		//test actions
 	}
